77cec7f9583b824d97ac993f792b65380250adc3
jyn <ggpp09@gmail.com>
2023-02-23 17:01:09 +0900

    기본주행v2 모터 동작 이상확인

--- line_trace.cpp
+++ line_trace.cpp
@@ -1,5 +1,7 @@
 #include "line_trace.h"
 #include "AFMotor.h"
+#include "Motor_move.h"
+#define 
 
 // 매개변수로 모터객체를 넣어서 그 모터가 검은색을 감지하면 true를 리턴하는 함수
 bool is_detect(int motor) {
@@ -10,27 +12,36 @@ bool is_detect(int motor) {
 int last_state = 0;
 
 void line_trace(AF_DCMotor &L, AF_DCMotor &R, int Lpin, int Rpin) {
+  int speed = 50;
   if (is_detect(Lpin) && is_detect(Rpin)) 
     // AF_DCMotor L, AF_DCMotor R, int speed_L, int speed_R, bool left_front, bool right_front
     {
-      MV(L, R, 160, 160, true, true);
-      Serial.println("both");
+      MV(L, R, speed, speed, true, true);
+      Serial.print(analogRead(Lpin));
+      Serial.print(" ");
+      Serial.println(analogRead(Rpin));
     }
 
   else if (!is_detect(Lpin) && is_detect(Rpin)) {
     // 오른쪽만 감지시 우회전
-    MV(L, R, 90, 160, true, true);
+    MV(L, R, speed, 0, true, true);
     last_state = 0;
-    Serial.println("right");
+    Serial.print(analogRead(Lpin));
+    Serial.print(" ");
+    Serial.println(analogRead(Rpin));
   } 
   else if (is_detect(Lpin) && !is_detect(Rpin)) {
-    MV(L, R, 160, 90, true, true);
+    MV(L, R, 0, speed, true, true);
     last_state = 1;
-    Serial.println("left");
+    Serial.print(analogRead(Lpin));
+    Serial.print(" ");
+    Serial.println(analogRead(Rpin));
   }
   else {
-    Serial.println("no");
-    if (last_state == 0) MV(L, R, 90, 200, true, true);
-    else if (last_state == 1) MV(L, R, 200, 90, true, true);
+    Serial.print(analogRead(Lpin));
+    Serial.print(" ");
+    Serial.println(analogRead(Rpin));
+    if (last_state == 0) MV(L, R, speed, 0, true, true);
+    else if (last_state == 1) MV(L, R, 0, speed, true, true);
   }
 }
