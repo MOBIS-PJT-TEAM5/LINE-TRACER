30be5484e4756dbda1e6f4f14f6dff6cb434a842
LeeSeyeong <39327058+LeeSeyeong@users.noreply.github.com>
2023-02-24 08:58:45 +0900

    매개변수 수정 및 속도 상승 값 multiple -> plus로

--- SmartCruise.cpp
+++ SmartCruise.cpp
@@ -1,17 +1,21 @@
 #include "SmartCruise.h"
+#include <SoftwareSerial.h>
+#include <Arduino.h>
 
-#define max_speed 120;
-#define slow_mulitple 0.2;
-#define fast_multiple 1.2;
+#define max_speed 120
+#define slow_mulitple 0.2
+#define fast_plus 5
+#define IR_sensor_threshold 500
 //참조 _ network slowstart
 
-int SmartCruise ( int now_speed ){
+
+int SmartCruise ( int now_speed, int IR_Sensor ){
   int change_speed = now_speed;
-  if ( analogRead(IR_sensor) < 500 ){
+  if ( analogRead (IR_Sensor) < IR_sensor_threshold ){
     change_speed *= slow_mulitple;
     return change_speed;
   } else {
-    change_speed *= fast_multiple;
+    change_speed += fast_plus;
     change_speed = (change_speed > max_speed) ?  max_speed : change_speed;
     return change_speed;
   }
