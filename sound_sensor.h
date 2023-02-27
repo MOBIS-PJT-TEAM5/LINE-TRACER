4ec01a1ffa77dddf7f6b08b69ef631df85944806
LeeSeyeong <39327058+LeeSeyeong@users.noreply.github.com>
2023-02-27 11:12:56 +0900

    semi final _ sound for video

--- sound_sensor.h
+++ sound_sensor.h
@@ -4,15 +4,18 @@
 #include <Arduino_AVRSTL.h>
 #include "Motor_move.h"
 #include "AFMotor.h"
+#include "line_trace.h"
 #include "config.h"
 
 /**
  * 스마트 크루즈 기능 (앞 차와의 간격 유지)
  * @param L 왼쪽 모터 객체
  * @param R 오른쪽 모터 객체
+ * @param Lpin 라인 감지 왼쪽 센서 번호
+ * @param Rpin 라인 감지 오른쪽 센서 번호
  * @param Sound_Sensor 사운드 감지 센서 번호
  */
 
-void hear_emergency(AF_DCMotor &Lmotor, AF_DCMotor &Rmotor, int Sound_Sensor);
+void hear_emergency(AF_DCMotor &L, AF_DCMotor &R, int Lpin, int Rpin, int Sound_Sensor);
 
 # endif
